159951dff359eb51dd8a1eb26e4a0f2cc2805ec8
James Knight <james.d.knight@live.com>
2019-02-23 02:23:02 -0500

    updating development snapshot (9494942; proper)
    Update documentation to use a new development snapshot [1] of the
    releng-tool documentation.
    
    [1]: https://github.com/releng-tool/releng-tool/commit/949494238c3bd5bd169055dc502aae6bae1499e5
    
    Signed-off-by: James Knight <james.d.knight@live.com>

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -3,7 +3,7 @@ var DOCUMENTATION_OPTIONS = {
     VERSION: '',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
-    FILE_SUFFIX: '',
+    FILE_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
     NAVIGATION_WITH_KEYS: false,
