bd83d72433a9f7773c58c80a2455c9a2a11694a7
James Knight <james.d.knight@live.com>
2019-02-26 02:20:22 -0500

    redirect.js: adding automatic redirects for new languages
    With the recently added (limited) documentation sets for the German and
    Japanese languages, adding respective automatic redirection support.
    
    Signed-off-by: James Knight <james.d.knight@live.com>

--- redirect.js
+++ redirect.js
@@ -3,6 +3,8 @@ var base = '/en/master';
 var ln = window.navigator.language || navigator.browserLanguage;
 switch(ln)
 {
+case 'de': base = '/de/master'; break;
+case 'jp': base = '/jp/master'; break;
 default:
     // use default
 }
