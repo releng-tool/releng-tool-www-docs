4478b1c82e1cef6150705502f89838f00ee50005
James Knight <james.d.knight@live.com>
2019-02-26 02:41:04 -0500

    redirect using first two-characters of language value
    The provided language value may have a verify specific language
    definition; however, only a limited set of language documentation
    support exists. Assume generic language types for now.
    
    Signed-off-by: James Knight <james.d.knight@live.com>

--- redirect.js
+++ redirect.js
@@ -1,6 +1,8 @@
 var base = '/en/master';
 
 var ln = window.navigator.language || navigator.browserLanguage;
+ln = ln.substring(0,2);
+console.log(ln);
 switch(ln)
 {
 case 'de': base = '/de/master'; break;
@@ -11,4 +13,5 @@ default:
 
 new_url = base + window.location.pathname + window.location.search +
     window.location.hash;
+console.log(new_url);
 window.location.href = new_url;
