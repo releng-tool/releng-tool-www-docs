45679748dfec484036976e984ac420118b6d43ea
James Knight <james.d.knight@live.com>
2019-02-26 02:44:10 -0500

    redirect.js: remove unwanted console logs
    Removing console logs in redirect script accidentally added.
    
    Signed-off-by: James Knight <james.d.knight@live.com>

--- redirect.js
+++ redirect.js
@@ -2,7 +2,6 @@ var base = '/en/master';
 
 var ln = window.navigator.language || navigator.browserLanguage;
 ln = ln.substring(0,2);
-console.log(ln);
 switch(ln)
 {
 case 'de': base = '/de/master'; break;
@@ -13,5 +12,4 @@ default:
 
 new_url = base + window.location.pathname + window.location.search +
     window.location.hash;
-console.log(new_url);
 window.location.href = new_url;
